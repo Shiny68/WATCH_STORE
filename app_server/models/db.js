a8ae16e4ee09d011553000ec9f7770c6f979962d
Shiny68 <21eg106b11@anurag.edu.in>
2023-09-22 12:08:16 +0530

    third commit

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,61 +1,35 @@
 const mongoose = require('mongoose');
-const readLine = require('readline');
+const dbURI = 'mongodb://127.0.0.1:27017';
 
-let dbURL = 'mongodb://localhost:27017/';
-if (process.env.NODE_ENV ==='production'){
-dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
-}
-
-const connect = () =>{
-setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true,
-useUnifiedTopology: true }), 1000);
-}
+mongoose.connect(dbURI, {useNewUrlParser: true});
 
 mongoose.connection.on('connected', () => {
-console.log('connected');
+ console.log(`Mongoose connected to ${dbURI}`);
 });
 
 mongoose.connection.on('error', err => {
-console.log('error:' + err);
-return connect();
+ console.log(`Mongoose connection error: ${err}`);
 });
 
-mongoose.connection.on('disconnected',() => {
-console.log('disconnected');
+mongoose.connection.on('disconnected', () => {
+ console.log('Mongoose disconnected');
 });
 
-if (process.platform === 'win32') {
-const rl = readLine.createInterface({
-input: process.stdin,
-
-output: process.stdout
-});
-rl.on ('SIGINT',() => {
-process.emit("SIGINT");
-});
-}
-
-const gracefulShutdown = (msg, callback) =>{
-mongoose.connection.close( () => {
-console.log(`Mongoose disconnected through ${msg}`);
-callback();
-});
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
 };
-
+// For nodemon restarts
 process.once('SIGUSR2', () => {
-gracefulShutdown('nodemon restart', () => {
-process.kill(process.pid, 'SIGUSR2');
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
-});
+// For app termination
 process.on('SIGINT', () => {
-gracefulShutdown('app termination', () => {
-process.exit(0);
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
 });
-});
-process.on('SIGTERM', () => {
-gracefulShutdown('Heroku app shutdown', () => {
-process.exit(0);
-});
-});
-connect();
-require('./locations');
